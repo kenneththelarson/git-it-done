75231d63a189ce537e66ca9956f9bb6321c40024
Kenny <kenneththelarson@gmail.com>
2020-12-10 22:25:01 -0700

    error catching

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,13 +9,21 @@ var getUserRepos = function (user) {
 
     // make a request to the url
     fetch(apiUrl).then(function (response) {
-        response.json().then(function (data) {
-            displayRepos(data, user);
-        });
+        // request was successful
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to GitHub");
     });
 };
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
     var username = nameInputEl.value.trim();
 
@@ -27,7 +35,12 @@ var formSubmitHandler = function(event) {
     };
 };
 
-var displayRepos = function(repos, searchTerm) {
+var displayRepos = function (repos, searchTerm) {
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
     console.log(repos);
     console.log(searchTerm);
 
